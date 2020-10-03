4810719805261fbac6a69441c67e7ca86bcd4060
Gabriel Silva <gabrielsos99@gmail.com>
2020-10-02 23:20:11 -0300

    feat: created login controller

---
+++ src/controllers/loginController.ts
@@ -0,0 +1,48 @@
+import { Request, Response } from 'express';
+import db from '../database/connection';
+
+import randomDigits from '../utils/crypto';
+
+export default class LoginController {
+  async index(request: Request, response: Response) {
+    const users = await db('users').select('*');
+
+    return response.json(users);
+  }
+
+  async newPassword(request: Request, response: Response) {
+    const { loginName, password } = request.body;
+
+    try {
+      await db('users')
+        .update('password', password)
+        .where('loginName', loginName);
+
+      return response.status(200).json(loginName);
+    } catch {
+      return response
+        .status(400)
+        .json({ erro: 'Não foi possível mudar sua senha' });
+    }
+  }
+
+  async create(request: Request, response: Response) {
+    const { loginName, email, name } = request.body;
+    const password = randomDigits();
+
+    try {
+      await db('users').insert({
+        loginName,
+        name,
+        email,
+        password,
+      });
+
+      return response.json({ loginName, email, name });
+    } catch {
+      return response
+        .status(400)
+        .json({ erro: 'Usuário ou e-mail já cadastrado' });
+    }
+  }
+}
