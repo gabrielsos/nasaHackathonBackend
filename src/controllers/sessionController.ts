6b38b49beab5aa1239d7313f130e450ed8f24719
Gabriel Silva <gabrielsos99@gmail.com>
2020-10-02 23:20:25 -0300

    feat: created session controller

---
+++ src/controllers/sessionController.ts
@@ -0,0 +1,26 @@
+﻿import { Request, Response } from 'express';
+import db from '../database/connection';
+
+interface User {
+  loginName: string;
+  name: string;
+  email: string;
+}
+
+export default class SessionController {
+  async create(request: Request, response: Response) {
+    const { loginName, password } = request.body;
+
+    const user: User = await db('users')
+      .where('loginName', loginName)
+      .where('password', password)
+      .select('loginName', 'name', 'email')
+      .first();
+
+    if (!user) {
+      return response.json({ error: 'Credenciais inválidas' });
+    }
+
+    return response.json(user);
+  }
+}
